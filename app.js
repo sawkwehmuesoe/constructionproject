d5fbf1c577633aef045375110b9572487ebb3884
Samuel <sawkwehmuesoe@gmail.com>
2023-06-24 01:34:30 +0630

    end at the info times

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 let navbar = document.querySelector('.header .navbar');
+let searchForm = document.querySelector('.header .search-form');
+let loginForm = document.querySelector('.header .login-form');
+let contactInfo = document.querySelector('.contact-info');
 
 document.querySelector('#menu-btn').onclick = ()=>{
     navbar.classList.toggle('active');
 }
 
+document.querySelector('#search-btn').onclick = ()=>{
+    searchForm.classList.toggle('active');
+}
+
+document.querySelector('#login-btn').onclick = ()=>{
+    loginForm.classList.toggle('active');
+}
+
+document.querySelector('#info-btn').onclick = ()=>{
+    contactInfo.classList.toggle('active');
+}
+
+document.querySelector('#close-contact-info').onclick = ()=>{
+    contactInfo.classList.remove('active');
+}
+
 window.onscroll = ()=>{
     navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    contactInfo.classList.remove('active');
 }
