0ea358276ecf73bb50df48521c846d7fc1f24dcf
Samuel <sawkwehmuesoe@gmail.com>
2023-07-05 00:38:17 +0630

    end banner section to put side arrow

--- app.js
+++ app.js
@@ -5,22 +5,37 @@ let contactInfo = document.querySelector('.contact-info');
 
 document.querySelector('#menu-btn').onclick = ()=>{
     navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    contactInfo.classList.remove('active');
 }
 
 document.querySelector('#search-btn').onclick = ()=>{
     searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+    contactInfo.classList.remove('active');
 }
 
 document.querySelector('#login-btn').onclick = ()=>{
     loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    contactInfo.classList.remove('active');
 }
 
 document.querySelector('#info-btn').onclick = ()=>{
     contactInfo.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
 }
 
 document.querySelector('#close-contact-info').onclick = ()=>{
     contactInfo.classList.remove('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
 }
 
 window.onscroll = ()=>{
